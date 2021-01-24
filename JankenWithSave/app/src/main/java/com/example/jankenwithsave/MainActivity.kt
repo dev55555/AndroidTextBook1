6f94c754ea244e41358de650fc52390b7ac12c58
TestUser <toshix.ishix.developer@outlook.jp>
2021-01-24 18:02:54 +0900

    Commit 2021/1/24 18:02

---
+++ JankenWithSave/app/src/main/java/com/example/jankenwithsave/MainActivity.kt
@@ -0,0 +1,31 @@
+package com.example.jankenwithsave
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.preference.PreferenceManager
+import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
+import org.jetbrains.anko.startActivity
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        // イベントリスナを設定
+        gu.setOnClickListener { onJankenButtonTapped(it) }
+        choki.setOnClickListener { onJankenButtonTapped(it) }
+        pa.setOnClickListener { onJankenButtonTapped(it) }
+
+        //
+        val pref = PreferenceManager.getDefaultSharedPreferences(this)
+        val editor = pref.edit()
+        editor.clear().apply()
+    }
+
+    // ボタンを押した時に実行するメソッド
+    fun onJankenButtonTapped(view: View?) {
+        startActivity<ResultActivity>("MY_HAND" to view?.id)
+    }
+}
