e661d5a4f3c75323ef70ddf0324ac2ec8d82d136
TestUser <toshix.ishix.developer@outlook.jp>
2021-02-28 18:21:17 +0900

    テキスト２冊目
    テキスト２冊目

--- Calculator/app/src/main/java/com/example/calculator/ResultActivity.kt
+++ Calculator/app/src/main/java/com/example/calculator/ResultActivity.kt
@@ -2,10 +2,22 @@ package com.example.calculator
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class ResultActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_result)
+100
+        val price = intent.getIntExtra("price", 0)
+        val discount = intent.getIntExtra("discount", 0)
+
+        val expression = findViewById<TextView>(R.id.expression_label)
+        expression.text = getString(R.string.expression, price, discount)
+
+        val discountedPrice = price * (100 - discount) / 100
+
+        val result = findViewById<TextView>(R.id.result_label)
+        result.text = getString(R.string.result, discountedPrice)
     }
 }
--- ListView/app/src/main/java/com/example/listview/MainActivity.kt
+++ ListView/app/src/main/java/com/example/listview/MainActivity.kt
@@ -2,10 +2,39 @@ package com.example.listview
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.ArrayAdapter
+import android.widget.ListView
+import android.widget.Toast
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // リストで表示するタイムゾーンの一覧
+        val timeZones = TimeZone.getAvailableIDs()
+        Log.d("MainActivity", "合計：${timeZones.size}")
+
+        // リストをレイアウトから探す
+        val listView = findViewById<ListView>(R.id.timeZoneList)
+
+        // アダプターを作成
+        val adapter = ArrayAdapter<String>(this,
+                R.layout.list_time_zone_row,
+                R.id.timeZone,
+                timeZones)
+
+        // リストにアダプターをセットする
+        listView.adapter = adapter
+
+        // リストのアイテムタップ時の動作
+        listView.setOnItemClickListener { parent, view, position, id ->
+            // アダプターから、押された位置のタイムゾーンを得る
+            val timeZone = adapter.getItem(position)
+            // Toastで表示
+            Toast.makeText(this, timeZone, Toast.LENGTH_SHORT).show()
+        }
     }
 }
